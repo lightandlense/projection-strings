79af1dd6c904d2f23df22c76ebaadd0aeb050a2f
zelkirb <russklimas@gmail.com>
2026-06-09 11:19:46 -0600

    fix: mirror for facing-wall â right hand = right (high pitch) strings

--- src/hand-tracker.js
+++ src/hand-tracker.js
@@ -55,7 +55,7 @@ export class HandTracker {
     }
 
     const tip  = results.multiHandLandmarks[0][8]; // index fingertip
-    const x    = tip.x * this.canvasWidth;
+    const x    = (1 - tip.x) * this.canvasWidth;   // mirror for facing-wall use: right hand = right strings
     const y    = tip.y * this.canvasHeight;
     const prevX = this.prevX ?? x;
     this.prevX  = x;
